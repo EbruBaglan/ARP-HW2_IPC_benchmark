d99e80638684d18bfb2c85eaf16712bb4d629633
Ebru Baglan <71343894+dolunaysuratusagi@users.noreply.github.com>
2022-02-10 16:21:21 +0100

    new zip, fix

--- install.sh
+++ install.sh
@@ -1,14 +1,14 @@
 clear
 pwd=$(pwd)
-unzip project_files -d /tmp/rere2
+unzip project_files -d /tmp/not
 mkdir $1
-mv -v /tmp/rere2/* $1
+mv -v /tmp/not/* $1
+rm -r /tmp/not
 
 export TESTVAR=$1
 cd $1
 
 gcc -o named named.c && gcc -o shared shared.c -lrt -lpthread
-
 mkdir ./c_files
 mv named.c ./c_files && mv shared.c ./c_files
 
@@ -19,3 +19,10 @@ mkdir ./executables
 mv named ./executables && mv shared ./executables
 
 cd $pwd
+
+
+
+
+
+
+
